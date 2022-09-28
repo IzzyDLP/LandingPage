f20410b55862bf3f82ac632f640432af02c2e997
Isaac De La Portilla <idlportilla@gmail.com>
2022-09-28 18:25:08 -0500

    finished the landing page. add a button for source code at the bottom in js.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-document.getElementById('source').addEventListener('click', function () {
-  function mySource() {
-    window.location.href = "https://github.com/IzzyDLP/LandingPage";
-  }
-})
+document.getElementById('source').addEventListener('click', function (mySource) {
+  window.location.href = 'https://github.com/IzzyDLP/LandingPage';
+}
+)
